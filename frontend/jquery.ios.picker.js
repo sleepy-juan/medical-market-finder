c7ee16574575b2f238e3f629efe15d348a297e7c
Hyunchang <hyunchang@kaist.ac.kr>
2022-08-23 04:24:46 +0900

    add picker wheel

---
+++ frontend/jquery.ios.picker.js
@@ -0,0 +1,238 @@
+
+
+(function ($) {
+
+    //    $("head").append("<style type='text/css'></style>");
+
+
+    $.fn.picker = function (json, callback) {
+        var options = json.data;
+        var lineHeight = 30;
+        $ele = $(this);
+        $ele.empty();
+        $ele.addClass("picker-wrapper");
+        $ele.append('<div class="clone-scroller"></div>');
+        $ele.append('<div class="picker-up"></div>');
+        $ele.append('<div class="picker-down"></div>');
+        $ele.append('<div class="picker-scroller"></div>');
+
+        if (typeof json.lineHeight != "undefined") {
+            lineHeight = json.lineHeight;
+        }
+        $.each(options, function (index, option) {
+            $ele.find('.clone-scroller').append('<div class="option">' + option + '</div>');
+            $ele.find('.picker-scroller').append('<div class="option">' + option + '</div>');
+        });
+        $ele.find('.clone-scroller').bind("scroll", function () {
+
+            clockWise(lineHeight);
+        });
+        $ele.find(".clone-scroller").bind("scrollstop", function (e) {
+            var scrollAmount = Math.round($(this).scrollTop() / lineHeight) * lineHeight;
+            $(this).parent().find(".clone-scroller").animate({
+                scrollTop: scrollAmount
+            }, 100);
+            
+            
+            
+            var eIndex = Math.round(unit / 22.5);
+            var $scroller = $(".picker-scroller");
+    var $clone = $(".clone-scroller");
+    var $cloneScrollTop = $(".clone-scroller").scrollTop();
+    var $options = $scroller.find(".option");
+    var $optionsNo = $options.length;
+    var $cloneHeight = lineHeight * $optionsNo;
+    var totalDeg = 22.5 * $optionsNo;
+    var unit = totalDeg / $cloneHeight * $cloneScrollTop;
+            
+            unit = Math.round(unit/22.5)*22.5;
+    //    $(".output").html(totalDeg + "/" + $cloneHeight + "/" + $cloneScrollTop);
+    $scroller.css("-webkit-transform", "translateZ(-90px) rotateX(" + unit + "deg)");
+            
+            
+        });
+
+        /*setting css*/
+        if (typeof json.lineHeight != "undefined") {
+            $ele.css("height", (lineHeight * 7) + "px");
+            $ele.css("line-height", lineHeight + "px");
+            $ele.find('.clone-scroller').css({
+                "padding-top": (lineHeight * 3) + "px",
+                "padding-bottom": (lineHeight * 3) + "px"
+            });
+            $ele.find('.picker-scroller').css({
+                "padding-top": (lineHeight * 3) + "px",
+                "padding-bottom": (lineHeight * 3) + "px"
+            });
+            $ele.find(".picker-up").css("height", (lineHeight * 3) + "px");
+            $ele.find(".picker-down").css("height", (lineHeight * 3) + "px");
+            $ele.find(".picker-down").css("top", (lineHeight * 4) + "px");
+        }
+        // default selected
+        if (typeof json.selected != "undefined") {
+            $ele.find('.clone-scroller').scrollTop(lineHeight * json.selected);
+            $ele.find('.picker-scroller').scrollTop(lineHeight * json.selected);
+        }
+
+        $ele.find('.picker-scroller').find(".option").each(function (index, $option) {
+            $option = $($option);
+            $option.css("-webkit-transform", "rotateX(-" + (22.5 * index) + "deg) translateZ(90px)");
+            if (index > 7) {
+                $option.hide();
+            }
+        });
+
+    };
+}(jQuery));
+
+
+var deg = 0;
+
+function clockWise(lineHeight) {
+    var $scroller = $(".picker-scroller");
+    var $clone = $(".clone-scroller");
+    var $cloneScrollTop = $(".clone-scroller").scrollTop();
+    var $options = $scroller.find(".option");
+    var $optionsNo = $options.length;
+    var $cloneHeight = lineHeight * $optionsNo;
+    var totalDeg = 22.5 * $optionsNo;
+    var unit = totalDeg / $cloneHeight * $cloneScrollTop;
+    //    $(".output").html(totalDeg + "/" + $cloneHeight + "/" + $cloneScrollTop);
+    $scroller.css("-webkit-transform", "translateZ(-90px) rotateX(" + unit + "deg)");
+
+    var eIndex = Math.round(unit / 22.5);
+    $(".output").html(eIndex);
+    $($options).hide();
+    $($options.get(eIndex)).show();
+    for (i = eIndex; i < (eIndex + 7); i++) {
+        $($options.get(i)).show();
+    }
+    if (eIndex > 7) {
+        for (i = eIndex; i >= (eIndex - 7); i--) {
+            $($options.get(i)).show();
+        }
+    } else {
+        for (i = 0; i < 8; i++) {
+            $($options.get(i)).show();
+        }
+    }
+
+}
+
+
+function clockWise2(lineHeight) {
+    var $scroller = $(".picker-scroller2");
+    var $clone = $(".clone-scroller2");
+    var $cloneScrollTop = $(".clone-scroller2").scrollTop();
+    var $options = $scroller.find(".option2");
+    var $optionsNo = $options.length*2;
+    var $cloneHeight = lineHeight * $optionsNo;
+    var totalDeg = 22.5 * $optionsNo*2;
+    var unit = totalDeg / $cloneHeight * $cloneScrollTop;
+    //    $(".output").html(totalDeg + "/" + $cloneHeight + "/" + $cloneScrollTop);
+    $scroller.css("-webkit-transform", "translateZ(-90px) rotateX(" + unit*0.5 + "deg)");
+
+    var eIndex = Math.round(unit / 22.5*0.5);
+    $(".output2").html(eIndex);
+    $($options).hide();
+    $($options.get(eIndex)).show();
+    for (i = eIndex; i < (eIndex + 7); i++) {
+        $($options.get(i)).show();
+    }
+    if (eIndex > 7) {
+        for (i = eIndex; i >= (eIndex - 7); i--) {
+            $($options.get(i)).show();
+        }
+    } else {
+        for (i = 0; i < 8; i++) {
+            $($options.get(i)).show();
+        }
+    }
+
+}
+
+
+(function ($) {
+
+    //    $("head").append("<style type='text/css'></style>");
+
+
+    $.fn.picker2 = function (json, callback) {
+        var options = json.data;
+        var lineHeight = 30;
+        $ele = $(this);
+        $ele.empty();
+        $ele.addClass("picker-wrapper2");
+        $ele.append('<div class="clone-scroller2"></div>');
+        $ele.append('<div class="picker-up2"></div>');
+        $ele.append('<div class="picker-down2"></div>');
+        $ele.append('<div class="picker-scroller2"></div>');
+
+        if (typeof json.lineHeight != "undefined") {
+            lineHeight = json.lineHeight;
+        }
+        $.each(options, function (index, option) {
+            $ele.find('.clone-scroller2').append('<div class="option2">' + option + '</div>');
+            $ele.find('.picker-scroller2').append('<div class="option2">' + option + '</div>');
+        });
+        $ele.find('.clone-scroller2').bind("scroll", function () {
+
+            clockWise2(lineHeight);
+        });
+        $ele.find(".clone-scroller2").bind("scrollstop", function (e) {
+            var scrollAmount = Math.round($(this).scrollTop() / lineHeight) * lineHeight;
+            $(this).parent().find(".clone-scroller2").animate({
+                scrollTop: scrollAmount*5
+            }, 100);
+            
+            
+            
+            var eIndex = Math.round(unit / 22.5);
+            var $scroller = $(".picker-scroller2");
+    var $clone = $(".clone-scroller2");
+    var $cloneScrollTop = $(".clone-scroller2").scrollTop();
+    var $options = $scroller.find(".option2");
+    var $optionsNo = $options.length;
+    var $cloneHeight = lineHeight * $optionsNo;
+    var totalDeg = 22.5 * $optionsNo;
+    var unit = totalDeg / $cloneHeight * $cloneScrollTop*2;
+            
+            unit = Math.round(unit/22.5)*22.5;
+    //    $(".output").html(totalDeg + "/" + $cloneHeight + "/" + $cloneScrollTop);
+    $scroller.css("-webkit-transform", "translateZ(-90px) rotateX(" + unit + "deg)");
+            
+            
+        });
+
+        /*setting css*/
+        if (typeof json.lineHeight != "undefined") {
+            $ele.css("height", (lineHeight * 7) + "px");
+            $ele.css("line-height", lineHeight + "px");
+            $ele.find('.clone-scroller2').css({
+                "padding-top": (lineHeight * 3) + "px",
+                "padding-bottom": (lineHeight * 3) + "px"
+            });
+            $ele.find('.picker-scroller2').css({
+                "padding-top": (lineHeight * 3) + "px",
+                "padding-bottom": (lineHeight * 3) + "px"
+            });
+            $ele.find(".picker-up2").css("height", (lineHeight * 3) + "px");
+            $ele.find(".picker-down2").css("height", (lineHeight * 3) + "px");
+            $ele.find(".picker-down2").css("top", (lineHeight * 4) + "px");
+        }
+        // default selected
+        if (typeof json.selected != "undefined") {
+            $ele.find('.clone-scroller2').scrollTop(lineHeight * json.selected);
+            $ele.find('.picker-scroller2').scrollTop(lineHeight * json.selected);
+        }
+
+        $ele.find('.picker-scroller2').find(".option2").each(function (index, $option) {
+            $option = $($option);
+            $option.css("-webkit-transform", "rotateX(-" + (22.5 * index) + "deg) translateZ(90px)");
+            if (index > 7) {
+                $option.hide();
+            }
+        });
+
+    };
+}(jQuery));
--- frontend/result.js
+++ frontend/result.js
@@ -1,60 +1,69 @@
-const ctx = document.getElementById('lines').getContext('2d');
-const lines = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '러시아의 수출액',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+// country1: 벨기에, country2: 러시아, scoreArray: [1년수출액,2년수출액,3년수출액,...], yearArray: [2011,2012,2013,...]
+// 모든 Array의 길이는 같아야한다. scoreArray[n]은 yearArray[n]년의 데이터이다.
+function makeLines(country1,country2,scoreArray1,scoreArray2){
+    const ctx = document.getElementById('lines').getContext('2d');
+    const lines = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: yearArray,
+            datasets: [{
+                label: country1+'의 수출액',
+                data: scoreArray1,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            },
+            {
+                label: country2+'의 수출액',
+                data: scoreArray2,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            },
+        ]
         },
-        {
-            label: '벨기에의 수출액',
-            data: [2, 9, 13, 3, 9, 4],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        },
-    ]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
         }
-    }
-});
+    });
+}
+
+
+
+
+
 
 
 const pieArea1 = document.getElementById('pie1').getContext('2d');
